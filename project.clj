7dd57ce87b277ae9b83f16ac74625431c8415525
Ziyun Li <me@ziyunli.dev>
2021-05-11 23:43:53 -0400

    project setup

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject predicates "1.0.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [iloveponies.tests/predicates "0.2.0-SNAPSHOT"]]
-  :profiles {:dev {:plugins [[lein-midje "3.2.1"]]}})
+  :profiles {:dev {:plugins [[lein-midje "3.2.1"]
+                             [lein-cljfmt "0.7.0"]]}})
